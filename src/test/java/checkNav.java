54a158e7f6821db8354ade082e69868b1218da56
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2025-02-10 15:30:21 +0400

    change and add MobileBy

--- src/test/java/checkNav.java
+++ src/test/java/checkNav.java
@@ -1,32 +1,38 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
+import io.appium.java_client.MobileBy;
+
 
 
 public class checkNav extends methods {
 
     @Test(priority = 1)
-    public void Agents() throws InterruptedException
-    {Thread.sleep(3000);
-    clicks(By.xpath("//android.widget.ImageView[@content-desc=\"Team\"]"));;
+    public void Team() throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement teamNav = wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("BOTTOM_NAVIGATION_BAR_TEAM\nTeam")));
+        teamNav.click();
     }
     @Test(priority = 2)
     public void Properties(){
-        clicks(By.xpath("//android.widget.ImageView[@content-desc=\"Listing\"]"));
+        WebElement propNav = wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("BOTTOM_NAVIGATION_BAR_LISTING\nListing")));
+        propNav.click();
     }
     @Test(priority = 3)
     public void Home(){
-        clicks(By.xpath("//android.widget.ImageView[@content-desc=\"Home\"]"));
-    }
+        WebElement homenav = wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("BOTTOM_NAVIGATION_BAR_HOME\nHome")));
+        homenav.click();    }
     @Test(priority = 4)
     public void Services(){
-        clicks(By.xpath("//android.widget.ImageView[@content-desc=\"Services\"]"));
-    }
+        WebElement servnav = wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("BOTTOM_NAVIGATION_BAR_SERVICES\nServices")));
+        servnav.click();    }
     @Test(priority = 5)
     public void Settings(){
-        clicks(By.xpath("//android.widget.ImageView[@content-desc=\"More\"]"));
-    }
+        WebElement setnav = wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("BOTTOM_NAVIGATION_BAR_MORE\nMore")));
+        setnav.click();    }
     @Test(priority = 6)
     public void Login(){
-        clickdifferent(By.xpath("(//android.view.View[@content-desc=\"Login\"])[2]"));
-    }
+        WebElement lognav = wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("PROFILE_LOGIN_BUTTON\nLogin")));
+        lognav.click();    }
 }
--- src/test/java/methods.java
+++ src/test/java/methods.java
@@ -33,7 +33,7 @@ public class methods {
 //        cap.setCapability("appActivity", "com.aqary.aqary_investment.MainActivity");
         URL url = new URL("http://localhost:4723");
         driver = new AndroidDriver(url, cap);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
     @AfterSuite
     public void tearDownSuite() throws InterruptedException {
